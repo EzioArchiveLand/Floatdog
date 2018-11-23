83971f14ed5dc6ffc87c4dc45659bb916c6b0963
jonathan <jedijon567@gmail.com>
2018-11-22 20:44:56 -0500

    write the basic outline

--- src/main/java/com/eziosoft/floatdog/Main.java
+++ src/main/java/com/eziosoft/floatdog/Main.java
@@ -5,5 +5,14 @@ public class Main {
     // main class
     public static void main(){
         System.out.println("Floatdog is starting up...");
+        System.out.println("Checking if Floatzel is ready to go...");
+        // TODO: add check to see if a copy of the floatzel JAR is present
+        if (1 == 0){
+            // do nothing for now
+            System.out.println("Unfinished feature");
+        } else {
+            System.out.println("Floatzel is ready to go!");
+        }
+        // TODO: execute the jar
     }
 }
